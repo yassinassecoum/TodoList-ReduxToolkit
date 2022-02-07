241385c5fae45c94c591c7261d6727f09e49a890
yassinassecoum <yassin.assecoum@hotmail.com>
2022-02-07 11:19:02 +0100

    add counter of completed items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
 
 function App() {
+  const completedTodos = useSelector((state) =>
+    state.todos.value.filter((todo) => todo.completed === true)
+  );
   return (
     <div className="App">
       <header className="App-header">
         <h1>TodoList React ReduxToolkit</h1>
         <AddTodo />
         <TodoList />
+        Total Complete Items: {completedTodos.length}
       </header>
     </div>
   );
